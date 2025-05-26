0d7d01b642cda817639b798a9e8cf79f896dec79
Misal Abro <abromisal384@gmail.com>
2025-05-26 23:44:59 +0500

    axiosInstance.js

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/blog',
+  baseURL: 'https://blog-backend-1-ay9b.onrender.com/api/blog',
   withCredentials: true,
 });
 
